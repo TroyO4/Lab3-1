cd6462cfd7e4427ebf7a387d08eb2773eaa0e5cf
DzialoC <dzialoc@go.stockton.edu>
2023-10-11 15:49:32 -0400

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String args[]) {
-        System.out.println("hello, " + arg[0] + "!");
+        System.out.println("hello, " + args[0] + "!");
     }
 }
